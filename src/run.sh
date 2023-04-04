fa3076abd2b9b91e8c00a362e082658291199213
Aman Gupta Karmani <aman@tmm1.net>
2023-04-03 21:50:46 -0700

    run in loop for docker

--- src/run.sh
+++ src/run.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 mkdir -p $DIR/data
+
+while true; do
 rm -rf $DIR/basic.xml
 
 python3 $DIR/ical_2_xmltv.py "https://calendar.google.com/calendar/ical/mg877fp19824mj30g497frm74o%40group.calendar.google.com/public/basic.ics" "TWiT.tv"
 
 mv $DIR/basic.xml $DIR/data/
+sleep 86400
+done
