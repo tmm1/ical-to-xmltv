c4bf206af9736905c3b4ac0b06aeac0f192732a4
Aman Karmani <aman@tmm1.net>
2023-04-04 10:00:22 -0700

    more logging

--- src/run.sh
+++ src/run.sh
@@ -8,8 +8,11 @@ mkdir -p $DIR/data
 while true; do
 rm -rf $DIR/basic.xml
 
+echo "Downloading latest iCal data..."
 python3 $DIR/ical_2_xmltv.py "https://calendar.google.com/calendar/ical/mg877fp19824mj30g497frm74o%40group.calendar.google.com/public/basic.ics" "TWiT.tv"
 
 mv $DIR/basic.xml $DIR/data/
+echo done.
+
 sleep 86400
 done
